c27b030fd5e4156b0b935ee95c213165fd6f1e33
Sriram2004yadav <151672116+Sriram2004yadav@users.noreply.github.com>
2024-08-27 16:29:42 +0530

    Add files via upload

---
+++ ead4/calculator.js
@@ -0,0 +1,21 @@
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+function backspace() {
+    let display = document.getElementById("display").value;
+    document.getElementById("display").value = display.substring(0, display.length - 1);
+}
+
+function calculateResult() {
+    try {
+        let result = eval(document.getElementById("display").value);
+        document.getElementById("display").value = result;
+    } catch (error) {
+        document.getElementById("display").value = "Error!";
+    }
+}
